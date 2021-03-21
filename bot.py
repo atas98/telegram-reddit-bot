9193f0d2858904d0346c7378ec3a2a007dd0887d
atas <andy.torkin@gmail.com>
2021-03-21 16:16:19 +0200

    Changed to webhooks

--- bot.py
+++ bot.py
@@ -1,7 +1,10 @@
-from utils import load_config
-from pathlib import Path
-import logging
 import os
+import ssl
+import logging
+from aiohttp import web
+from pathlib import Path
+from utils import load_config
+from aiogram.dispatcher.webhook import get_new_configured_app
 
 if __name__ == '__main__':
     # Configure logging
@@ -16,11 +19,23 @@ if __name__ == '__main__':
         exit(f"Error: {ex}")
 
     # Now load everything else
-    from aiogram import executor
-    from misc import dp
-    from handlers import register_bot_commands
+    # from aiogram import executor
+    from misc import dp, WEBHOOK_URL_PATH, on_startup, on_shutdown
+    from utils.load_config import CONFIG
 
-    # TODO: Switch polling to webhooks
-    executor.start_polling(dp,
-                           skip_updates=True,
-                           on_startup=register_bot_commands)
+    # Get instance of :class:`aiohttp.web.Application` with configured router.
+    app = get_new_configured_app(dispatcher=dp, path=WEBHOOK_URL_PATH)
+
+    # Setup event handlers.
+    app.on_startup.append(on_startup)
+    app.on_shutdown.append(on_shutdown)
+
+    # Generate SSL context
+    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    context.load_cert_chain(CONFIG.ssl.CERT, CONFIG.ssl.PRIV)
+
+    # Start web-application.
+    web.run_app(app,
+                host=CONFIG.webapp.HOST,
+                port=CONFIG.webapp.PORT,
+                ssl_context=context)
--- misc.py
+++ misc.py
@@ -1,12 +1,52 @@
-# from bot import CONFIG
+import asyncio
 from utils.load_config import CONFIG
 from aiogram import Bot, Dispatcher
-# from aiogram.contrib.fsm_storage.redis import RedisStorage2
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.fsm_storage.redis import RedisStorage2
+# from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from controllers.reddit import Reddit
+from handlers import register_bot_commands
+
+loop = asyncio.get_event_loop()
+WEBHOOK_URL = f"https://{CONFIG.webhook.HOST}:{CONFIG.webhook.PORT}{CONFIG.webhook.URL_PATH}"
 
 # Initialize bot and dispatcher
 reddit = Reddit(CONFIG.reddit.client_id, CONFIG.reddit.client_secret,
                 CONFIG.reddit.user_agent)
-bot = Bot(token=CONFIG.telegramToken, parse_mode="HTML", validate_token=True)
-dp = Dispatcher(bot, storage=MemoryStorage())
+bot = Bot(token=CONFIG.telegramToken,
+          parse_mode="HTML",
+          validate_token=True,
+          loop=loop)
+dp = Dispatcher(bot,
+                storage=RedisStorage2(host=CONFIG.redis.HOST,
+                                      port=CONFIG.redis.PORT,
+                                      db=CONFIG.redis.DB,
+                                      password=CONFIG.redis.PASSWORD))
+
+
+async def on_startup():
+    await register_bot_commands()
+
+    # Get current webhook status
+    webhook = await bot.get_webhook_info()
+
+    # If URL is bad
+    if webhook.url != WEBHOOK_URL:
+        # If URL doesnt match current - remove webhook
+        if not webhook.url:
+            await bot.delete_webhook()
+
+        # Set new URL for webhook
+        await bot.set_webhook(WEBHOOK_URL,
+                              certificate=open(CONFIG.ssl.CERT, 'rb'))
+
+
+async def on_shutdown():
+    """
+    Graceful shutdown. This method is recommended by aiohttp docs.
+    """
+    # Remove webhook.
+    await bot.delete_webhook()
+
+    # Close Redis connection.
+    await dp.storage.close()
+    await dp.storage.wait_closed()
